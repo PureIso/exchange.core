927731984d7617c0232e593285e58ad2cb5c81a7
Ola <olaegby@live.com>
2020-05-21 22:20:13 +0300

    initial work
    initial work

--- exchange.signalR.client.web.backend/Startup.cs
+++ exchange.signalR.client.web.backend/Startup.cs
@@ -71,6 +71,12 @@ namespace exchange.signalR.client.web.backend
             {
                 endpoints.MapControllers();
             });
+            app.UseSpa(spa =>
+            {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+                spa.Options.SourcePath = "wwwroot";
+            });
         }
         #endregion
     }
