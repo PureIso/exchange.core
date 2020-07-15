1d4454e001f5cb258c3f91c30c04dbdf2c2ee786
Ola <olaegby@live.com>
2020-07-15 19:27:25 +0300

    initial commit

--- exchange.core/Interfaces/IExchangeService.cs
+++ exchange.core/Interfaces/IExchangeService.cs
@@ -18,45 +18,5 @@ namespace exchange.core.interfaces
         Action<MessageType, string> ProcessLogBroadcast { get; set; }
         #endregion
 
-        #region Public Properties
-        public ServerTime ServerTime { get; set; }
-        Dictionary<string, decimal> CurrentPrices { get; set; }
-        List<Ticker> Tickers { get; set; }
-        List<Account> Accounts { get; set; }
-        List<Product> Products { get; set; }
-        List<HistoricRate> HistoricRates { get; set; }
-        List<Fill> Fills { get; set; }
-        List<BinanceFill> BinanceFill { get; set; }
-        List<Order> Orders { get; set; }
-        OrderBook OrderBook { get; set; }
-        Product SelectedProduct { get; set; } 
-        List<AccountHistory> AccountHistories { get; set; } 
-        List<AccountHold> AccountHolds { get; set; }
-        #endregion
-
-        #region Methods
-
-        Task<BinanceAccount> UpdateBinanceAccountAsync();
-        Task<ExchangeInfo> UpdateExchangeInfoAsync();
-        Task<ServerTime> UpdateTimeServerAsync();
-        Task<List<Account>> UpdateAccountsAsync(string accountId="");
-        Task<List<AccountHistory>> UpdateAccountHistoryAsync(string accountId);
-        Task<List<AccountHold>> UpdateAccountHoldsAsync(string accountId);
-        Task<List<Order>> UpdateOrdersAsync(Product product = null);
-        Task<Order> PostOrdersAsync(Order order);
-        Task<List<Order>> CancelOrderAsync(Order order);
-        Task<List<Order>> CancelOrdersAsync(Product product);
-        Task<List<Product>> UpdateProductsAsync();
-        Task<List<Ticker>> UpdateTickersAsync(List<Product> products);
-        Task<List<Fill>> UpdateFillsAsync(Product product);
-        Task<List<BinanceFill>> UpdateBinanceFillsAsync(Product product);
-        Task<OrderBook> UpdateProductOrderBookAsync(Product product, int level = 2);
-        Task<BinanceOrder> BinancePostOrdersAsync(BinanceOrder order);
-        Task<BinanceOrder> BinanceCancelOrdersAsync(BinanceOrder binanceOrder);
-        Task<List<HistoricRate>> UpdateProductHistoricCandlesAsync(Product product, DateTime startingDateTime, DateTime endingDateTime, int granularity = 86400);
-        Task<bool> CloseFeed();
-        bool ChangeFeed(string message);
-        void StartProcessingFeed();
-        #endregion
     }
 }
