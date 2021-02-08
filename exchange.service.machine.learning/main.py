0fd4e515f515d2723558bc32588ab328f7b45419
Ola <olaegby@live.com>
2021-02-08 11:10:37 +0200

    Update main.py

--- exchange.service.machine.learning/main.py
+++ exchange.service.machine.learning/main.py
@@ -1,8 +1,7 @@
-import os
+"""Main Entrypoint"""
 from flask_restful import Api
-from flask import Flask, request
+from flask import Flask
 from app.exchange import Exchange
-from app.views.home import Home
 from app.views.predict import Predict
 from app.views.task_status import TaskStatus
 from app.views.recurrent_neural_network import RecurrentNeuralNetwork
@@ -11,9 +10,15 @@ if __name__ == '__main__':
     exchange = Exchange()
     application = Flask(__name__, static_url_path='/static')
     api = Api(application)
-    api.add_resource(Home, '/')
-    api.add_resource(RecurrentNeuralNetwork, '/api/v1/rnn/',resource_class_kwargs={'exchange': exchange})
-    api.add_resource(TaskStatus, '/api/v1/taskstatus/',resource_class_kwargs={'exchange': exchange})
-    api.add_resource(Predict, '/api/v1/predict/',resource_class_kwargs={'exchange': exchange})
+    rnn_routes = [
+        '/',
+        '/api/v1/rnn/',
+    ]
+    api.add_resource(RecurrentNeuralNetwork, *rnn_routes,
+                     resource_class_kwargs={'exchange': exchange})
+    api.add_resource(TaskStatus, '/api/v1/taskstatus/',
+                     resource_class_kwargs={'exchange': exchange})
+    api.add_resource(Predict, '/api/v1/predict/',
+                     resource_class_kwargs={'exchange': exchange})
     exchange.mi_logger.info('Machine Learning Service Starting')
     application.run(host=exchange.mi_host, port=exchange.mi_port, debug=False)
