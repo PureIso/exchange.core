5f98558aa3e81b6a267478b4a170994735a207d7
Ola <olaegby@live.com>
2021-02-11 11:53:21 +0200

    Update main.py

--- exchange.service.machine.learning/main.py
+++ exchange.service.machine.learning/main.py
@@ -1,24 +1,26 @@
 """Main Entrypoint"""
 from flask_restful import Api
 from flask import Flask
-from app.exchange import Exchange
+from app.configuration import Configuration
 from app.views.predict import Predict
 from app.views.task_status import TaskStatus
 from app.views.recurrent_neural_network import RecurrentNeuralNetwork
 
 if __name__ == '__main__':
-    exchange = Exchange()
+    configuration = Configuration()
     application = Flask(__name__, static_url_path='/static')
     api = Api(application)
+    # route initialization
     rnn_routes = [
         '/',
         '/api/v1/rnn/',
     ]
     api.add_resource(RecurrentNeuralNetwork, *rnn_routes,
-                     resource_class_kwargs={'exchange': exchange})
+                     resource_class_kwargs={'configuration': configuration})
     api.add_resource(TaskStatus, '/api/v1/taskstatus/',
-                     resource_class_kwargs={'exchange': exchange})
+                     resource_class_kwargs={'configuration': configuration})
     api.add_resource(Predict, '/api/v1/predict/',
-                     resource_class_kwargs={'exchange': exchange})
-    exchange.mi_logger.info('Machine Learning Service Starting')
-    application.run(host=exchange.mi_host, port=exchange.mi_port, debug=False)
+                     resource_class_kwargs={'configuration': configuration})
+    configuration.mi_logger.info('Machine Learning Service Starting')
+    # application initialization
+    application.run(host=configuration.mi_host, port=configuration.mi_port, debug=False)
