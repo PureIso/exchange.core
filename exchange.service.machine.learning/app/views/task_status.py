791bd58f624a4125fbf9498a281038f0beb9224e
Ola <olaegby@live.com>
2021-02-14 21:05:36 +0200

    Update task_status.py

--- exchange.service.machine.learning/app/views/task_status.py
+++ exchange.service.machine.learning/app/views/task_status.py
@@ -1,41 +1,43 @@
-import sys
-from app.tasks.task_work import training
+"""Celery Task Status"""
 from flask_restful import Resource, reqparse
-from flask import Response, jsonify, json, request
+from flask import Response, json
+from app.tasks.task_work import training
+
 
 class TaskStatus(Resource):
-    def __init__(self,exchange):
-        self.exchange = exchange
+    """Task Status class
+    Args:
+        Resource (Resource): RESTFul resource
+    """
+    def __init__(self, configuration):
+        self.configuration = configuration
         self.reqparse = reqparse.RequestParser()
         self.reqparse.add_argument('task_id', type=str, location='values')
         super(TaskStatus, self).__init__()
 
     def get(self):
-        try:
-            args = self.reqparse.parse_args()
-            task_id = str(args['task_id'])
-            task = self.exchange.celery.AsyncResult(id=task_id, app=training)
+        """Task Status [get] method
+        Returns:
+            Response: The response
+        """
+        args = self.reqparse.parse_args()
+        task_id = str(args['task_id'])
+        task = self.configuration.celery.AsyncResult(id=task_id, app=training)
 
-            if task.state == 'PENDING':
-                # job did not start yet
-                response = {
-                    'state': task.state,
-                    'status': 'Pending...'
-                }
-            else:
-                response = {
-                    'state': task.state,
-                    'status': str(json.dumps(str(task.info))),
-                }
-            message = json.dumps(response)
-            self.exchange.mi_logger.info(message)
-            response = Response(message,
-                                status=200,  # Status OK
-                                mimetype='application/json')
-        except:
-            message = json.dumps({"error": str(sys.exc_info())})
-            self.exchange.mi_logger.error(message)
-            response = Response(message,
-                                status=500,  # Status Internal Server Error
-                                mimetype='application/json')
+        if task.state == 'PENDING':
+            # job did not start yet
+            response = {
+                'state': task.state,
+                'status': 'Pending...'
+            }
+        else:
+            response = {
+                'state': task.state,
+                'status': str(json.dumps(str(task.info))),
+            }
+        message = json.dumps(response)
+        self.configuration.mi_logger.info(message)
+        response = Response(message,
+                            status=200,  # Status OK
+                            mimetype='application/json')
         return response
